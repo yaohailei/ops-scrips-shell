cba75951c3c817a1708c849dd560675c58e2276d
layne <layne_yao@outlook.com>
2019-09-17 14:26:47 +0800

    比较dns解析结果，输出不同的dnsname

--- differ_dig.sh
+++ differ_dig.sh
@@ -2,10 +2,10 @@
 # 将处理成列的dnsname输入到文件中
 cat tmp.txt.bak|while read line
 do
-  v6=`d1ig @2001:da8:1031:414::9 ${line}.nufe.edu.cn|egrep -v '^$'| egrep -v '^;'|grep -w -E "IN"|grep -w -E "A|AAAA"|awk '{print $5}'|sort|uniq|tr "\n" "|"`
-  v4=`dig @210.28.81.1 ${line}.nufe.edu.cn|egrep -v '^$'| egrep -v '^;'|grep -w -E "IN"|grep -w -E "A|AAAA"| awk '{print $5}'|sort|uniq |tr "\n" "|"`
+  v6=`dig +short @2001:da8:1031:414::9 ${line}.nufe.edu.cn`
+  v4=`dig +short @210.28.81.1 ${line}.nufe.edu.cn`
 #  echo "$v4"
   if [ "$v6" != "$v4" ];then
-    echo "$line no"
+    echo "$line"
   fi
 done
